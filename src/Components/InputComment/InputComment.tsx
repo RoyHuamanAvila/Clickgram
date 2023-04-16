f2c71367fa0913a4c94e6d2da337bdc718c71c23
Roy Huaman <RHavila789@gmail.com>
2023-04-16 16:16:00 -0500

    feat: white-space

--- src/Components/InputComment/InputComment.tsx
+++ src/Components/InputComment/InputComment.tsx
@@ -5,22 +5,33 @@ export interface InputCommentProps { }
 
 const InputComment: React.FC<InputCommentProps> = () => {
 	const [comment, setComment] = useState<string>('');
+	const [newComments, setNewComments] = useState<string[]>([]);
 
 	const handleUpdateComment = (e: FormEvent<HTMLTextAreaElement>) => {
 		setComment(e.currentTarget.value);
 	}
 
+	const handleAddComment = (comment: string) => {
+		setNewComments(newComments.concat(comment));
+	}
+
 	const handleSendComment = () => {
-		let newComment = removeExtraSpaces(comment);
-		console.log(newComment)
+		let trimmedComment = removeExtraSpaces(comment);
+		handleAddComment(trimmedComment);
+		setComment('')
 	}
 
 	return (
 		<div className='inputcomment dark'>
-			<textarea title='Comment' placeholder='Agrega un commentario...' onChange={handleUpdateComment} value={comment} rows={1} />
 			{
-				verifyTextNotBlank(comment) && <button onClick={handleSendComment}>Comment</button>
+				newComments.map((comment, index) => <p key={index}>{comment}</p>)
 			}
+			<div className="inputcomment-input">
+				<textarea title='Comment' placeholder='Agrega un commentario...' onChange={handleUpdateComment} value={comment} rows={1} />
+				{
+					verifyTextNotBlank(comment) && <button onClick={handleSendComment}>Comment</button>
+				}
+			</div>
 		</div >
 	);
 };
