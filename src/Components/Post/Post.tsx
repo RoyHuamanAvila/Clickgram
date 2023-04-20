215fb022bfb9739b7e059967d23d645abeb6c9d3
Roy Huaman <RHavila789@gmail.com>
2023-04-20 11:25:54 -0500

    feat: comment component and userpicture component

--- src/Components/Post/Post.tsx
+++ src/Components/Post/Post.tsx
@@ -1,7 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import './styles/Post.scss';
 import { PostProps } from '../../interfaces';
-import { formatNumber } from '../../utils/formats';
 import { InputComment } from '../InputComment';
 import { Carousel } from '../Carousel';
 import { PostModal } from '../PostModal';
--- src/Components/PostHeader/PostHeader.tsx
+++ src/Components/PostHeader/PostHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/PostHeader.scss';
 import { User } from '../../interfaces';
+import { UserPicture } from '../UserPicture';
 export interface PostHeaderProps {
 	owner: User;
 }
@@ -9,9 +10,7 @@ const PostHeader: React.FC<PostHeaderProps> = ({ owner }) => {
 	return (
 		<div className="post-header">
 			<div className="post-ownerinfo">
-				<div className="ownerinfo-picture">
-					<img src={owner.picture} alt="post owner" />
-				</div>
+				<UserPicture picture={owner.picture} />
 				<p className='ownerinfo-name'>{owner.name}</p>
 			</div>
 			<i className="bi bi-three-dots fs-5"></i>
