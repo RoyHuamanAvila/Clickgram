04c642b54ccd29a3d0577f65d10539612d39a1ea
Roy Huaman <RHavila789@gmail.com>
2023-04-25 22:15:36 -0500

    feat: button comments open modal

--- src/Components/PostInteractive/PostInteractive.tsx
+++ src/Components/PostInteractive/PostInteractive.tsx
@@ -6,16 +6,17 @@ import { LikeButtonProps } from '../LikeButton/LikeButton';
 import { formatNumber } from '../../utils/formats';
 export interface PostInteractiveProps {
 	likeButton: LikeButtonProps;
+	idPost: string;
 }
 
-const PostInteractive: React.FC<PostInteractiveProps> = ({ likeButton }) => {
+const PostInteractive: React.FC<PostInteractiveProps> = ({ likeButton, idPost }) => {
 	const { handleLikePost, liked, likeCounter } = likeButton;
 	return (
 		<div className="post-interactive">
 			<div className='post-interactive-buttons'>
 				<div>
 					<LikeButton handleLikePost={handleLikePost} liked={liked} likeCounter={likeCounter} />
-					<i className="bi bi-chat comments"></i>
+					<button type='button' className="bi bi-chat btn-showcomments" data-bs-toggle="modal" data-bs-target={`#${idPost}modal`} title='View comments'></button>
 					<i className="bi bi-send"></i>
 				</div>
 				<SavePostButton saved={false} />
