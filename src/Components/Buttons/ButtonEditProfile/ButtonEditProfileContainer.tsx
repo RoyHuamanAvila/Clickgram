8deb01c72ad61ffb0c743c931ddc9565233a36ca
Roy Huaman <RHavila789@gmail.com>
2023-05-02 09:17:36 -0500

    feat: Button edit profile

---
+++ src/Components/Buttons/ButtonEditProfile/ButtonEditProfileContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ButtonEditProfile from './ButtonEditProfile'
+
+const ButtonEditProfileContainer = () => {
+  return <ButtonEditProfile />
+}
+
+export default ButtonEditProfileContainer
--- src/Pages/Profile/ProfileContainer.tsx
+++ src/Pages/Profile/ProfileContainer.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import Profile from "./Profile"
 import { UserLogged } from "../../interfaces"
-import { useAppDispatch } from "../../hooks";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 import { axiosGetUser, axiosGetUserByUsername } from "../../features/user/userSlice";
 import { useParams } from "react-router-dom";
 import axios from "axios";
@@ -10,6 +10,8 @@ const ProfileContainer = () => {
   const [user, setUser] = useState<UserLogged>();
   const dispatch = useAppDispatch();
   const { username } = useParams();
+  const userLogged = useAppSelector(state => state.user.data);
+  const [isOwner, setIsOwner] = useState<boolean>(false);
 
   const handleGetUser = async () => {
     const response = await axios.get(`http://localhost:3000/user/username/${username}`);
@@ -19,9 +21,10 @@ const ProfileContainer = () => {
 
   useEffect(() => {
     if (username) handleGetUser();
+    setIsOwner(userLogged.username === username)
   }, [username])
 
-  return <Profile user={user} />
+  return <Profile user={user} isOwner={isOwner} />
 }
 
 export default ProfileContainer
