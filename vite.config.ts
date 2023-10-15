c3acff4a5c780cec0bcb2826bcf73b9e3d8b79cc
Roy Huaman <RHavila789@gmail.com>
2023-10-15 02:13:30 -0500

    feat: add dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react({ fastRefresh: false })],
   test: {
     environment: "happy-dom",
-    setupFiles: ["./src/test/setup.ts", "dotenv/config"],
+    setupFiles: ["./src/test/setup.ts", "./node_modules/dotenv/config"],
     testTimeout: 35000,
   },
   worker: {
